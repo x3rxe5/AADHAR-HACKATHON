e9170662a5dba7aca7c0b3f01d03904a7d7d4843
xerxes <xerxes@pop-os.localdomain>
2021-10-29 00:11:43 +0530

    added logic for formatting address issues

--- src/logic/parsing.py
+++ src/logic/parsing.py
@@ -6,11 +6,18 @@ from src.schemas.schema import CreateAddress
 
 def parse_address(res:CreateAddress) -> dict:
     parser_dict = dict(res)
-    address_list = list()
-    address_set = set()
-    for address in parser_dict:
-        address_list.append(address)
-        address_set.add(parser_dict[address])
-    # print(address_list)
-    print(address_set)
-    return res
+    print("This is parsed dictionary -> ",parser_dict)
+    res_dict = main_logic(parser_dict)
+    return res_dict
+
+# main logic implementation for parsing and combine the address
+def main_logic(input_dict:dict) -> dict:
+    values = set(list(input_dict.values()))
+    output_dict = {}    
+    for v in values:        
+        res = [k for k, z in input_dict.items() if z == v]
+        if len(res) > 1:
+            output_dict['/'.join(res)] = v
+        else:
+            output_dict[res[0]] = v
+    return output_dict
--- src/routers/routes.py
+++ src/routers/routes.py
@@ -4,7 +4,7 @@ from src.logic.parsing import parse_address
 
 route = APIRouter()
 
-@route.get("/",response_model=CreateAddress)
+@route.post("/")
 async def check_address(res:CreateAddress):
     res_dict = parse_address(res)
     return res_dict
