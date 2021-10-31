9fc3ec4998d6a8207120d565b37a6dc5480177d9
xerxes <xerxes@pop-os.localdomain>
2021-10-31 16:04:31 +0530

    Local Language Support Added

--- src/logic/parsing.py
+++ src/logic/parsing.py
@@ -1,14 +1,23 @@
-
-from typing import Optional
+from googletrans import Translator
 from typing import Optional
 from src.schemas.schema import CreateAddress
+from src.logic.language_dict import LANGUAGES
 
-
-def parse_address(res:CreateAddress) -> dict:
-    parser_dict = dict(res)
-    print("This is parsed dictionary -> ",parser_dict)
+def parse_address(res:CreateAddress,lang:Optional[str]=None):
+    parser_dict = dict(res)   
     res_dict = main_logic(parser_dict)
-    return res_dict
+    final_dict = dict()
+    if lang != None:
+        result_dict = dict(parse_address_local(res_dict,lang))
+        final_dict = {
+            "english":res_dict,
+            LANGUAGES[lang]:result_dict
+        }
+    else:
+        final_dict = {
+            "english":res_dict,
+        }
+    return final_dict
 
 # main logic implementation for parsing and combine the address
 def main_logic(input_dict:dict) -> dict:
@@ -21,3 +30,14 @@ def main_logic(input_dict:dict) -> dict:
         else:
             output_dict[res[0]] = v
     return output_dict
+
+def parse_address_local(res_dict:dict,lang:str) -> dict:
+    translator = Translator()
+    result_dict = dict()
+    for key in res_dict:
+        if isinstance(res_dict[key],str):
+            result_dict[key] = (translator.translate(res_dict[key], src='en', dest=lang)).text
+        else:
+            result_dict[key] = res_dict[key] 
+    
+    return result_dict
